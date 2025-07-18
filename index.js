60c5864a92b4083a4a721e38749a04126b184d78
hui <huixiankuang4@gmail.com>
2025-07-18 00:58:14 -0400

    fix timer accuracy, fix issue of not updating timer when pressing tab early of only entering 1 digit

--- index.js
+++ index.js
@@ -30,10 +30,12 @@ function btnHTML(eleSelector, text) {
 
 //index = of all the selected start/reset buttons; (index + 1) locates the id of the target element(#c1, #c2, #r1, #r2...)
 function reset(index) {
-  if (timerID, flashID) {
-    clearInterval(timerID)
+  if (timerID) clearInterval(timerID)
+  if (flashID) {
+    document.querySelector(`#c${index + 1}`).style.opacity = '1'
     clearInterval(flashID)
   }
+  
   btnHTML(`#s${index + 1}`, 'START')
   alarmSound.currentTime = 0
   alarmSound.pause()
@@ -64,7 +66,13 @@ workTimer.forEach((timer, index) => {
   timer.addEventListener('keydown', (e) => {
     if (e.key === 'Tab' && timer.contentEditable) {
       e.preventDefault()
-
+      //if user only input one number and hit 'tab', add a 0 to the beginning
+      if (timer.innerHTML.length === 1) {
+        timerRecord[index] = timer.innerHTML.padStart(2, '0')
+        timer.innerHTML = timerRecord[index]
+        console.log(timerRecord)
+      }
+      
       if (index < 3) {
         switchEnterSection(workTimer, index, 1)
       }
@@ -123,32 +131,58 @@ mainBtns.forEach((btn, index) => {
       btn.innerHTML = 'PAUSE'
       const displayMin = document.querySelector(`#c${index + 1} .min`)
       const displaySec = document.querySelector(`#c${index + 1} .sec`)
-      const [min, sec] = [
-        displayMin.innerHTML,
-        displaySec.innerHTML
-      ].map(Number)
-      let totalSec = min * 60 + sec
+      // const [min, sec] = [
+      //   displayMin.innerHTML,
+      //   displaySec.innerHTML
+      // ].map(Number)
+
+      // current timestamp + timer convert to ms = alarm timestamp
+      const getTime = (Number(timerRecord[index * 2]) * 60 + Number(timerRecord[index * 2 + 1])) * 1000
+      const end = Date.now() + getTime
       timerID = setInterval(() => {
-        if (totalSec > 0) {
-          isPause = false
-          totalSec--
-          const newMin = String(Math.floor(totalSec / 60)).padStart(2, '0')
-          const newSec = String(totalSec % 60).padStart(2, '0')
-          displayMin.innerHTML = `${newMin}`
-          displaySec.innerHTML = `${newSec}`
-        } else {
+        //sometimes timestamp we get from Date.now() can be larger than the real time due to lagging from cpu, browser, etc; Causing thr result being negative. use math.max to avoid it
+        const remainSec = Math.max(0, Math.round((end - Date.now()) / 1000))
+        console.log(remainSec)
+        const newMin = String(Math.floor(remainSec / 60)).padStart(2, '0')
+        const newSec = String(remainSec % 60).padStart(2, '0')
+        displayMin.innerHTML = newMin
+        displaySec.innerHTML = newSec
+        
+        if (remainSec === 0) {
           alarmSound.play()
           clearInterval(timerID)
           isPause = true
           btn.innerHTML = 'STOP'
-          // flash the timer when time is up;
-          const toFlash = document.querySelector(`#c${index + 1}`)
           flashID = setInterval(() => {
-            toFlash.style.opacity = (toFlash.style.opacity === "1") ? "0" : "1"
+            toFlash.style.opacity = (toFlash.style.opacity === "1") ? "0" : "1" 
           }, 800);
-          // if its the work timer,display "let's take a break"; otherwise display "time to get productive!"
         }
-      }, 1000)
+      }, 1000);
+      //bug: not counting correctly (slower than real time)
+      // timerID = setInterval(() => {
+      //   if (totalSec > 0) {
+      //     isPause = false
+      //     totalSec--
+      //     const newMin = String(Math.floor(totalSec / 60)).padStart(2, '0')
+      //     const newSec = String(totalSec % 60).padStart(2, '0')
+      //     displayMin.innerHTML = `${newMin}`
+      //     displaySec.innerHTML = `${newSec}`
+      //   } else {
+      //     alarmSound.play()
+      //     clearInterval(timerID)
+      //     isPause = true
+      //     btn.innerHTML = 'STOP'
+      //     // flash the timer when time is up;
+      //     const toFlash = document.querySelector(`#c${index + 1}`)
+      //     flashID = setInterval(() => {
+      //       toFlash.style.opacity = (toFlash.style.opacity === "1") ? "0" : "1"
+      //     }, 800);
+      //   }
+      // }, 1000)
+      // if its the work timer,display "let's take a break"; otherwise display "time to get productive!"
+      // ticking in <title>?
+      //bug: two timer can not work separetly (maybe just stack them into one container)
+      // add inspiring sentences to this website(API)
     } else {
       btn.innerHTML = 'START'
       clearInterval(timerID)
